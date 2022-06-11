822a9e5b8ab8f75be52c8c3b3342dd80f9ce2684
Carlos1200 <carlos1200herrera@gmail.com>
2022-06-10 21:08:15 -0600

    Changing coneccion string

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+//creating a new pool with the connection string
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "my_u_library",
-  password: process.env.DB_PASSWORD || "root",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
